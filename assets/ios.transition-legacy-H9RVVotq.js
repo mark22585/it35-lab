197ee6c2e1a7217071b4597f72503d6900aef112
mark22585 <markestevenbayanban@gmail.com>
2025-05-15 09:14:03 +0800

    Updates

--- assets/ios.transition-legacy-H9RVVotq.js
+++ assets/ios.transition-legacy-H9RVVotq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CndAhmM_.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DK4dxaO5.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-wNHMRquH.js
+++ assets/swipe-back-legacy-wNHMRquH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CndAhmM_.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-DK4dxaO5.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
